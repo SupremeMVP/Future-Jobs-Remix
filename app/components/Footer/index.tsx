899dd6a7830df3332d07a2f20ad87274f8b4fa81
Tim Skaggs <me@timskaggs.com>
2022-03-12 23:57:45 -0500

    [TS] Add to footer, add Bootstrap icons

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -1,8 +1,8 @@
 export default function Header() {
   return (
     <footer className="pt-5 my-5 text-muted border-top">
-      Created by <a href="https://timskaggs.com/">Tim ✌️</a><br />
-      Thanks to <a href="https://getbootstrap.com/docs/5.0/examples/starter-template/">Bootstrap template</a>, <a href="https://remix.run/docs/en/v1">Remix Docs</a>, <a href="https://fastapi.tiangolo.com/">FastAPI Docs</a>, <a href="https://docs.deta.sh/docs/home">Deta Docs</a>, and <a href="https://remix-cookbook.com/">Remix Cookbook</a>· © 2022
+      Created by <a href="https://timskaggs.com/">Tim ✌️</a>·<a href="https://github.com/SupremeMVP/Future-Jobs-Remix"><i className="bi bi-github"></i> Open Source Remix</a>·<a href="https://github.com/SupremeMVP/Future-Job-FastAPI"><i className="bi bi-github"></i> Open Source FastAPI</a><br />
+      Thanks to <a href="https://getbootstrap.com/docs/5.0/examples/starter-template/">Bootstrap template</a>·<a href="https://remix.run/docs/en/v1">Remix Docs</a>·<a href="https://fastapi.tiangolo.com/">FastAPI Docs</a>·<a href="https://docs.deta.sh/docs/home">Deta Docs</a>·<a href="https://remix-cookbook.com/">Remix Cookbook</a>·<a href="https://remix.guide/">Remix Guide</a>· © 2022
     </footer>
   );
 }
--- app/root.tsx
+++ app/root.tsx
@@ -70,6 +70,7 @@ export default function App() {
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous"></link>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css"></link>
         <Meta />
         <Links />
       </head>
